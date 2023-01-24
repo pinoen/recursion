b5bac1332bd088e062521d727c264b7086aa28c3
Emilio Pino <pinoen@yahoo.com.ar>
2023-01-24 18:25:00 -0300

    Refactor

--- mergeSort.js
+++ mergeSort.js
@@ -1,39 +1,38 @@
 function mergeSort(arr) {
-  if (arr.length === 0) return []
-  if (arr.length === 1) return arr;
+  if (arr.length <= 1) return arr;
 
-  const mid = Math.floor(arr.length / 2);
-  const leftSubArr = arr.slice(0, mid);
-  const rightSubArr = arr.slice(mid);
+  const midIndex = Math.floor(arr.length / 2);
+  const leftSubArr = arr.slice(0, midIndex);
+  const rightSubArr = arr.slice(midIndex);
 
   return merge(mergeSort(leftSubArr), mergeSort(rightSubArr));
 }
 
-function merge(arr1, arr2) {
+function merge(leftArr, rightArr) {
   const result = [];
-  let iL = 0;
-  let iR = 0;
+  let leftIndex = 0;
+  let rightIndex = 0;
 
-  while (iL < arr1.length && iR < arr2.length) {
-    if (arr1[iL] < arr2[iR]) {
-      result.push(arr1[iL]);
-      iL++;
+  while (leftIndex < leftArr.length && rightIndex < rightArr.length) {
+    if (leftArr[leftIndex] < rightArr[rightIndex]) {
+      result.push(leftArr[leftIndex]);
+      leftIndex++;
     } else {
-      result.push(arr2[iR]);
-      iR++;
+      result.push(rightArr[rightIndex]);
+      rightIndex++;
     }
   }
-  while (iL < arr1.length) {
-    result.push(arr1[iL]);
-    iL++;
+  while (leftIndex < leftArr.length) {
+    result.push(leftArr[leftIndex]);
+    leftIndex++;
   }
-  while (iR < arr2.length) {
-    result.push(arr2[iR]);
-    iR++;
+  while (rightIndex < rightArr.length) {
+    result.push(rightArr[rightIndex]);
+    rightIndex++;
   }
   return result;
 };
 
-console.log(mergeSort([]))
+console.log(mergeSort([54]))
 console.log(mergeSort([10, 7, 8, 77, -5, 9, 1, 5]));
 console.log(mergeSort([30, 20, 10, 50, 22, 33, 55, 58, 89, 74])); 
